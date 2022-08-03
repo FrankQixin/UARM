156e85f5f30792551a53798f8b828f33ac3889ad
D-Q-X <85398350+D-Q-X@users.noreply.github.com>
2022-08-03 16:47:48 -0600

    update

---
+++ Robomaster/website/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/navbar/Navbar";
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
